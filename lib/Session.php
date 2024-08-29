cc7ed888b18eb1583af51632055d1fa32f0a6517
Nababur Rahaman <nababur@users.noreply.github.com>
2024-08-30 03:28:40 +0600

    Update Session.php

--- lib/Session.php
+++ lib/Session.php
@@ -2,71 +2,68 @@
 
 // Class Name: Session
 
-class Session{
+class Session
+{
 
 
   // Session Start Method
-  public static function init(){
+  public static function init()
+  {
 
     if (version_compare(phpversion(), '5.4.0', '<')) {
       if (session_id() == '') {
         session_start();
       }
-    }else{
+    } else {
       if (session_status() == PHP_SESSION_NONE) {
         session_start();
       }
     }
-
-
-
   }
 
 
   // Session Set Method
-  public static function set($key, $val){
+  public static function set($key, $val)
+  {
     $_SESSION[$key] = $val;
   }
 
 
 
   // Session Get Method
-  public static function get($key){
+  public static function get($key)
+  {
     if (isset($_SESSION[$key])) {
       return $_SESSION[$key];
-    }else{
+    } else {
       return false;
     }
   }
 
   // User logout Method
-  public static function destroy(){
+  public static function destroy()
+  {
     session_destroy();
     session_unset();
-    header('Location:login.php');
+    echo "<script>window.location='login.php';</script>";
   }
 
 
   // Check Session Method
-  public static function CheckSession(){
+  public static function CheckSession()
+  {
     if (self::get('login') == FALSE) {
       session_destroy();
-      header('Location:login.php');
+      echo "<script>window.location='login.php';</script>";
     }
   }
 
 
   // Check Login Method
-  public static function CheckLogin(){
+  public static function CheckLogin()
+  {
     if (self::get("login") == TRUE) {
-      header('Location:index.php');
+      echo "<script>window.location='index.php';</script>";
     }
   }
-
-
-
-
-
-
-
 }
